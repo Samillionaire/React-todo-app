1edffd924de524b7a4d99e6e742867c40156237a
sameer15v3@gmail.com <98183896+Samillionaire@users.noreply.github.com>
2024-02-22 02:51:34 +0530

    minor change
    learned functionality of  useref

--- Projects/6-todo-app-version-three/src/Components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/Components/AddTodo.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import { IoAdd } from "react-icons/io5";
 
 function AddTodo({ onNewItem }) {
   const [todoName, setTodoName] = useState("");
   const [dueDate, setDueDate] = useState("");
+  const noOfUpdates = useRef(0);
   const handleNameChange = (event) => {
     setTodoName(event.target.value);
+    noOfUpdates.current += 1;
   };
   const handleDateChange = (event) => {
     setDueDate(event.target.value);
+    console.log(`no of updates are : ${noOfUpdates.current}`)
   };
-  const handleAddButtonClicked = () => {
-    onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+     onNewItem(todoName, dueDate);
+     setTodoName("");
+     setDueDate("");
   };
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
         <div className="col-6">
           <input
             type="text"
@@ -28,18 +32,18 @@ function AddTodo({ onNewItem }) {
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate}  onChange={handleDateChange} />
+          <input type="date" value={dueDate} onChange={handleDateChange} />
         </div>
         <div className="col-2">
           <button
-            type="button"
+            type="submit"
             className="btn btn-success kg-button"
-            onClick={handleAddButtonClicked}
+            
           >
-          <IoAdd />
+            <IoAdd />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
